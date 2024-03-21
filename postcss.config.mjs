d559ead68a49743ea6685ef84a445b8bcecf7e8f
Yudi <12040397+Yudi@users.noreply.github.com>
2024-03-21 20:55:40 -0300

    Add openlayers

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,9 @@
 import tailwind from "tailwindcss";
 import autoprefixer from "autoprefixer";
 import postcssMixins from "postcss-mixins";
+import postcssImport from "postcss-import";
 
 /** @type {import('postcss-load-config').Config} */
 export default {
-  plugins: [tailwind, autoprefixer, postcssMixins],
+  plugins: [tailwind, autoprefixer, postcssMixins, postcssImport],
 };
