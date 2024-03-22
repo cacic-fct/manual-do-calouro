b64f87ba64a72ccf7d9c6f2e2b4e324cb0bee398
Yudi <12040397+Yudi@users.noreply.github.com>
2024-03-21 21:29:42 -0300

    Add css nesting

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,8 +2,10 @@ import tailwind from "tailwindcss";
 import autoprefixer from "autoprefixer";
 import postcssMixins from "postcss-mixins";
 import postcssImport from "postcss-import";
+import postcssNesting from "postcss-nesting";
+import tailwindNesting from "tailwindcss/nesting/index.js";
 
 /** @type {import('postcss-load-config').Config} */
 export default {
-  plugins: [autoprefixer, postcssMixins, postcssImport, tailwind],
+  plugins: [postcssMixins, postcssImport, tailwindNesting(postcssNesting), tailwind, autoprefixer],
 };
