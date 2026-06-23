995a2a2ba8ec2be9c2708ad6dff6193ce31844dd
Yudi <12040397+Yudi@users.noreply.github.com>
2026-06-23 17:35:08 -0300

    Cookie banner

---
+++ src/theme/Root.js
@@ -0,0 +1,20 @@
+import { useEffect } from 'react';
+import { createCookieBanner } from '@cacic-fct/account-manager-cookie-banner';
+import '@cacic-fct/account-manager-cookie-banner/styles.css';
+
+export default function Root({ children }) {
+  useEffect(() => {
+    const banner = createCookieBanner({
+      privacyPolicyUrl: 'https://cacic.dev.br/legal/privacy-policy',
+      onAccept: () => {
+        window.dispatchEvent(new Event('cookieBannerAccepted'));
+      },
+    });
+
+    return () => {
+      banner.destroy();
+    };
+  }, []);
+
+  return <>{children}</>;
+}
